0ec2604b44aff523923e54c3cce9228306bad897
Roman Gemini <roman@homefs.biz>
2016-10-10 15:48:30 +0300

    style fix

--- tests/fixtures/services/TestResponseServiceProvider.php
+++ tests/fixtures/services/TestResponseServiceProvider.php
@@ -4,11 +4,9 @@ namespace tests\fixtures\services;
 
 use Nerd\Framework\Http\ResponseServiceContract;
 use Nerd\Framework\Providers\ServiceProvider;
-use tests\fixtures\services\TestResponseService;
 
 class TestResponseServiceProvider extends ServiceProvider
 {
-
     public function register()
     {
         $this->getApplication()->singleton(ResponseServiceContract::class, TestResponseService::class);
