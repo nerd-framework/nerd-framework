d332c427fa44d35eb0e6583dfd443d791d7f917e
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2016-10-15 21:32:56 +0300

    fix routing service mock

--- tests/fixtures/services/TestRoutingService.php
+++ tests/fixtures/services/TestRoutingService.php
@@ -19,4 +19,93 @@ class TestRoutingService implements RouterContract
 
         return 'hello';
     }
+
+    /**
+     * Add route for GET method into routes list.
+     *
+     * @param string $route
+     * @param callable $action
+     * @param mixed $data
+     * @return RouterContract
+     */
+    public function get($route, $action, $data = null)
+    {
+        return $this;
+    }
+
+    /**
+     * Add route for POST method into routes list.
+     *
+     * @param string $route
+     * @param callable $action
+     * @param mixed $data
+     * @return RouterContract
+     */
+    public function post($route, $action, $data = null)
+    {
+        return $this;
+    }
+
+    /**
+     * Add route for PUT method into routes list.
+     *
+     * @param string $route
+     * @param callable $action
+     * @param mixed $data
+     * @return RouterContract
+     */
+    public function put($route, $action, $data = null)
+    {
+        return $this;
+    }
+
+    /**
+     * Add route for DELETE method into routes list.
+     *
+     * @param string $route
+     * @param callable $action
+     * @param mixed $data
+     * @return RouterContract
+     */
+    public function delete($route, $action, $data = null)
+    {
+        return $this;
+    }
+
+    /**
+     * @param string $route
+     * @param callable $action
+     * @param null $data
+     * @return RouterContract
+     */
+    public function any($route, $action, $data = null)
+    {
+        return $this;
+    }
+
+    /**
+     * Add middleware to router.
+     *
+     * @param $route
+     * @param $middleware
+     * @return RouterContract
+     */
+    public function middleware($route, $middleware)
+    {
+        return $this;
+    }
+
+    /**
+     * Add route into routes list.
+     *
+     * @param string|array $methods
+     * @param string $regexp
+     * @param callable $action
+     * @param mixed $data
+     * @return RouterContract
+     */
+    public function add($methods, $regexp, $action, $data = null)
+    {
+        return $this;
+    }
 }
